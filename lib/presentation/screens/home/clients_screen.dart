c73445e1eaa0fb35a26580f1ee8767c478fdbe44
Luis Calle Lazo <callelazodeynarluis@gmail.com>
2024-12-23 22:05:31 -0400

    Client Screen

--- lib/presentation/screens/home/clients_screen.dart
+++ lib/presentation/screens/home/clients_screen.dart
@@ -1,10 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:san_andres_mobile/presentation/widgets/buttons/btn_float_dev.dart';
+import 'package:san_andres_mobile/presentation/widgets/card/card_client.dart';
 
-class ClientsScreen extends StatelessWidget {
+class ClientsScreen extends StatefulWidget {
   const ClientsScreen({super.key});
 
   @override
+  State<ClientsScreen> createState() => _ClientsScreenState();
+}
+
+class _ClientsScreenState extends State<ClientsScreen> {
+  bool _isExpanded = false;
+  late bool stateOptions;
+  _loadData() {}
+
+  @override
+  void initState() {
+    super.initState();
+    stateOptions = true;
+  }
+
+  void _toggleExpand() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Text("CLIENTES");
+    return Stack(
+      children: [
+        ListView(
+          children: const [
+            CardClient(),
+            CardClient(),
+            CardClient(),
+            CardClient(),
+            CardClient(),
+            CardClient(),
+            CardClient(),
+          ],
+        ),
+        if (_isExpanded)
+          GestureDetector(
+            onTap: _toggleExpand,
+            child: Container(
+              color: Colors.black.withOpacity(0.5),
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ),Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 20, bottom: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  if (_isExpanded) ...[
+                    BtnFloatDev(
+                      icon: Icons.add,
+                      text: "Agregar",
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 20),
+                    BtnFloatDev(
+                      icon: Icons.search_off_sharp,
+                      text: "Filtro",
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 20),
+                  ],
+                  // FAB principal para expandir o contraer
+                  stateOptions
+                      ? BtnFloatDev(
+                          text: "",
+                          onPressed: _toggleExpand,
+                          bg: Colors.red[900],
+                          icon: _isExpanded ? Icons.close : Icons.keyboard_arrow_up_rounded,
+                        )
+                      : const Spacer(),
+                ],
+              ),
+            ),
+          ),
+      ],
+    );
   }
 }
---
+++ lib/presentation/widgets/card/card_client.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class CardClient extends StatefulWidget {
+  const CardClient({super.key});
+
+  @override
+  State<CardClient> createState() => _CardClientState();
+}
+
+class _CardClientState extends State<CardClient> {
+  void _showOptions(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return const MenuForItemClient(clientId: 1,);
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _showOptions(context),
+      child: Card(
+        clipBehavior: Clip.hardEdge,
+        elevation: 10,
+        child: Container(
+          height: 100,
+          color: Colors.white60,
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Image.network(
+                'https://picsum.photos/id/1/600/250',
+                height: 80,
+                width: 80,
+                fit: BoxFit.cover,
+              ),
+              const Text(
+                "Jose Luis Lazo Nina",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+// Menu de 3 opciones por cada tarjeta de autoparte
+class MenuForItemClient extends StatelessWidget {
+  final int clientId;
+  const MenuForItemClient({
+    super.key,
+    required this.clientId,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+            leading: const Icon(Icons.edit, color: Colors.red),
+            title: const Text('Editar Información',
+                style: TextStyle(color: Colors.red)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.picture_as_pdf, color: Colors.red),
+            title: const Text('Generar recibo PDF', style: TextStyle(color: Colors.red)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.table_rows_rounded, color: Colors.red),
+            title: const Text('Generar recibo XLSX', style: TextStyle(color: Colors.red)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.delete, color: Colors.red),
+            title: const Text('Eliminar', style: TextStyle(color: Colors.red)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
