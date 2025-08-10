630c1a0817b1016f220eba9dbb9bd7872854e6d4
LuisCalleLazo <callelazodeynarluis@gmail.com>
2025-08-09 23:21:32 -0400

    feat(autopart-table): Add name

--- lib/infraestructure/database/tables/autopart_table.dart
+++ lib/infraestructure/database/tables/autopart_table.dart
@@ -3,6 +3,7 @@ import 'package:drift/drift.dart';
 class AutopartTable extends Table {
   IntColumn get id => integer().autoIncrement()();
   IntColumn get refId => integer().nullable()();
+  TextColumn get name => text().nullable()();
   IntColumn get brandId => integer()();
   IntColumn get categoryId => integer()();
 }
